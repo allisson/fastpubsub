d72c934fa39dcf3c1ffccb4d75707ce0ca82d98c
Allisson Azevedo <allisson@gmail.com>
2025-12-29 10:13:05 -0300

    feat: add the subscriptions api (#3)

--- fastpubsub/api.py
+++ fastpubsub/api.py
@@ -3,15 +3,18 @@ from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse, ORJSONResponse
 from gunicorn.app.base import BaseApplication
 
-from fastpubsub import services
+from fastpubsub import models, services
 from fastpubsub.config import settings
 from fastpubsub.exceptions import AlreadyExistsError, NotFoundError
-from fastpubsub.models import AlreadyExists, CreateTopic, ListTopicAPI, NotFound, Topic
 
 tags_metadata = [
     {
         "name": "topics",
         "description": "Operations with topics.",
+    },
+    {
+        "name": "subscriptions",
+        "description": "Operations with subscriptions.",
     },
 ]
 
@@ -25,52 +28,95 @@ app = FastAPI(
 
 @app.exception_handler(AlreadyExistsError)
 def already_exists_exception_handler(request: Request, exc: AlreadyExistsError):
-    response = jsonable_encoder(AlreadyExists(detail=exc.args[0]))
+    response = jsonable_encoder(models.AlreadyExists(detail=exc.args[0]))
     return JSONResponse(status_code=status.HTTP_409_CONFLICT, content=response)
 
 
 @app.exception_handler(NotFoundError)
 def not_found_exception_handler(request: Request, exc: NotFoundError):
-    response = jsonable_encoder(NotFound(detail=exc.args[0]))
+    response = jsonable_encoder(models.NotFound(detail=exc.args[0]))
     return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content=response)
 
 
 @app.post(
     "/topics",
-    response_model=Topic,
+    response_model=models.Topic,
     status_code=status.HTTP_201_CREATED,
-    responses={409: {"model": AlreadyExists}},
+    responses={409: {"model": models.AlreadyExists}},
     tags=["topics"],
 )
-def create_topic(data: CreateTopic):
+def create_topic(data: models.CreateTopic):
     return services.create_topic(data)
 
 
 @app.get(
     "/topics/{id}",
-    response_model=Topic,
+    response_model=models.Topic,
     status_code=status.HTTP_200_OK,
-    responses={404: {"model": NotFound}},
+    responses={404: {"model": models.NotFound}},
     tags=["topics"],
 )
 def get_topic(id: str):
     return services.get_topic(id)
 
 
-@app.get("/topics", response_model=ListTopicAPI, status_code=status.HTTP_200_OK, tags=["topics"])
+@app.get("/topics", response_model=models.ListTopicAPI, status_code=status.HTTP_200_OK, tags=["topics"])
 def list_topic(offset: int = 0, limit: int = 10):
     topics = services.list_topic(offset, limit)
-    return ListTopicAPI(data=topics)
+    return models.ListTopicAPI(data=topics)
 
 
 @app.delete(
     "/topics/{id}",
     status_code=status.HTTP_204_NO_CONTENT,
-    responses={404: {"model": NotFound}},
+    responses={404: {"model": models.NotFound}},
     tags=["topics"],
 )
 def delete_topic(id: str):
     services.delete_topic(id)
+
+
+@app.post(
+    "/subscriptions",
+    response_model=models.Subscription,
+    status_code=status.HTTP_201_CREATED,
+    responses={409: {"model": models.AlreadyExists}},
+    tags=["subscriptions"],
+)
+def create_subscription(data: models.CreateSubscription):
+    return services.create_subscription(data)
+
+
+@app.get(
+    "/subscriptions/{id}",
+    response_model=models.Subscription,
+    status_code=status.HTTP_200_OK,
+    responses={404: {"model": models.NotFound}},
+    tags=["subscriptions"],
+)
+def get_subscription(id: str):
+    return services.get_subscription(id)
+
+
+@app.get(
+    "/subscriptions",
+    response_model=models.ListSubscriptionAPI,
+    status_code=status.HTTP_200_OK,
+    tags=["subscriptions"],
+)
+def list_subscription(offset: int = 0, limit: int = 10):
+    subscriptions = services.list_subscription(offset, limit)
+    return models.ListSubscriptionAPI(data=subscriptions)
+
+
+@app.delete(
+    "/subscriptions/{id}",
+    status_code=status.HTTP_204_NO_CONTENT,
+    responses={404: {"model": models.NotFound}},
+    tags=["subscriptions"],
+)
+def delete_subscription(id: str):
+    services.delete_subscription(id)
 
 
 class CustomGunicornApp(BaseApplication):
